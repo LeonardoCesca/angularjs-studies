80f3537eeef9f2ba0188f9cc6be9138f4c7f05de
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-11 16:03:39 -0300

    ADD more condition into ellipsis pipe

--- curso-rodrigo-branas/js/filters/ellipsisFilter.js
+++ curso-rodrigo-branas/js/filters/ellipsisFilter.js
@@ -1,7 +1,7 @@
 angular.module("listaTelefonica").filter("ellipsis", function() {
     return function (input, size) {
         if(input.length <= size) return input;
-        let output = input.substring(0,size) + '...';
+        let output = input.substring(0,size || 2) + '...';
         return output;
     }
 });
