bacd194825e57a7b1c68607a606742071f583153
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-11 13:39:03 -0300

    ADD update provider and create config serial

--- curso-rodrigo-branas/js/services/serialGeneratorService.js
+++ curso-rodrigo-branas/js/services/serialGeneratorService.js
@@ -1,9 +1,19 @@
 angular.module("listaTelefonica").provider("serialGenerator", function () {
+    let _length = 10;
+    
+    this.getLength = function () {
+        return _length;
+    };
+
+    this.setLength = function (length) {
+        _length = length;
+    };
+
     this.$get = function () {
         return {
             generate: function () {
                 let serial = "";
-                while(serial.length < 20) {
+                while(serial.length < _length) {
                     serial += String.fromCharCode(Math.floor(Math.random() * 64) + 32);
                 }
                 return serial;
