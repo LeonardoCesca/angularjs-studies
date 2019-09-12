fa23b8be7ee1a812fa00f2dc3bdcf45acfd3d8a5
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-12 15:08:08 -0300

    ADD format date with regEx expression

---
+++ curso-rodrigo-branas/js/directives/uiDateDirective.js
@@ -0,0 +1,23 @@
+angular.module("listaTelefonica").directive("uiDate", function() {
+    return {
+        require: "ngModel",
+        link: function(scope, element, attrs, ctrl) {
+
+            let _formatDate = function (date) {
+                date = date.replace(/[^0-9]+/g, "");
+                if(date.length > 2) {
+                    date = date.substring(0,2) + "/" + date.substring(2);
+                }
+                if(date.length > 5) {
+                    date = date.substring(0,5) + "/" + date.substring(5, 9);
+                }
+                return date;
+            }
+
+            element.bind("keyup", function() {
+                ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
+                ctrl.$render();
+            });
+        }
+    };
+});
