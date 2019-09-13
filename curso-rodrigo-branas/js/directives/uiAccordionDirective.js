c9622d4f905e156141939f559c548b320bfa2942
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-13 14:31:47 -0300

    ADD new controller to manage when close or open modal

--- curso-rodrigo-branas/js/directives/uiAccordionDirective.js
+++ curso-rodrigo-branas/js/directives/uiAccordionDirective.js
@@ -1,3 +1,20 @@
+angular.module("listaTelefonica").directive("uiAccordions", function() {
+    return {
+        controller: function($scope, $element, $attrs) {
+            let accordions = [];
+            this.registerAccordions = function (scopeAccordion) {
+                accordions.push(scopeAccordion)
+            };
+
+            this.closeAll = function () {
+                accordions.forEach(function (accordion) {
+                    accordion.isOpen = false;
+                });
+            }
+        }
+    };
+});
+
 angular.module("listaTelefonica").directive("uiAccordion", function() {
     return {
         templateUrl: "view/accordion.html",
@@ -6,9 +23,12 @@ angular.module("listaTelefonica").directive("uiAccordion", function() {
         },
         transclude: true,
         link: function(scope, element, attrs, ctrl) {
+            ctrl.registerAccordions(scope);
             scope.open = function() {
-                scope.isOpen = !scope.isOpen;
+                ctrl.closeAll();
+                scope.isOpen = true;
             }
-        } 
+        },
+        require: "^uiAccordions"
     };
 });
