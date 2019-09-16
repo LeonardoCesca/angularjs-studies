d7a16e0a5eda890d0da81dcdce4c5d0c10c41ccc
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-16 16:41:48 -0300

    ADD test with multiply numbers and sum numbers

--- poc-tests-jest/src/SimpleTest/mathService.test.js
+++ poc-tests-jest/src/SimpleTest/mathService.test.js
@@ -18,5 +18,16 @@ describe('Math service - addTwoNumbers', function() {
         let atual = _mathService.addTwoNumbers(1,1);
         expect(atual).toEqual(2);
     });
+
+    it('2 + 3 should equal 5', function() {
+        let atual = _mathService.addTwoNumbers(2,3);
+        expect(atual).toEqual(5);
+    });
+
+    it('(2+3)*4 should equal 20', function() {
+        let soma = _mathService.addTwoNumbers(2,3);
+        let multiplicacao = _mathService.addMultiplyNumbers(soma, 4);
+        expect(multiplicacao).toEqual(20); 
+    });
     
 });
