9d3ad69fe0e15680643dfee6b645d8f1e8e6ca01
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-16 16:07:29 -0300

    ADD mathService.js with function to addTwoNumbers

---
+++ poc-tests-jest/src/SimpleTest/mathService.js
@@ -0,0 +1,11 @@
+angular.module('mathModule', [])
+    .factory('mathService', function() {
+        
+        let addTwoNumbers = function(num1, num2) {
+            return num1 + num2;
+        };
+
+        return {
+            addTwoNumbers
+        };
+    });
