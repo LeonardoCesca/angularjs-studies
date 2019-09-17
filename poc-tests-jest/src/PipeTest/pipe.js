b272349144db6db8339509cbe4785570b6fc5da2
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-17 14:44:34 -0300

    REMOVE pipeTest

--- poc-tests-jest/src/PipeTest/pipe.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('app', []); 
-	app.controller('geek', function($scope) { 
-		$scope.string1 = "geeksforgeeks is the computer science portal for geeks."; 
-	$scope.upper = function() { 
-		$scope.string2 = angular.uppercase($scope.string1); 
-	} 
-}); 
