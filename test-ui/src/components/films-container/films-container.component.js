91c3b1d25dc74cc300645d833299a4712d54afc9
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-19 10:17:09 -0300

    ADD films container component

---
+++ test-ui/src/components/films-container/films-container.component.js
@@ -0,0 +1,9 @@
+(function() {
+    "use strict";
+
+angular.module('app')
+    .component('filmsContainer', {
+        templateUrl: "./src/components/films-container/films-container.html"
+    });
+    
+})();
