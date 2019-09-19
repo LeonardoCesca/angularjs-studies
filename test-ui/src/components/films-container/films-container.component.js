9f8b47dd36cf79c13663d4f96108856669715c27
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-19 12:05:08 -0300

    UPDATE with consuming API (without binding)

--- test-ui/src/components/films-container/films-container.component.js
+++ test-ui/src/components/films-container/films-container.component.js
@@ -1,9 +1,32 @@
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-angular.module('app')
+  angular
+    .module('app')
     .component('filmsContainer', {
-        templateUrl: "./src/components/films-container/films-container.html"
+      templateUrl: "./src/components/films-container/films-container.html",
+      controller: filmsContainerController,
+      controllerAs: 'vm',
     });
-    
+
+  function filmsContainerController(
+    filmsApiFactory
+  ) {
+    const vm = this;
+
+    vm.films = undefined,
+    vm.erros = undefined,
+
+    vm.$onInit = init;
+
+    function init() {
+      filmsApiFactory
+        .getFilms()
+        .then(res => this.films = res.data.results)
+        .catch(err => console.log(err))
+        .finally(() => console.log(this.films));
+    }
+
+  }
+
 })();
