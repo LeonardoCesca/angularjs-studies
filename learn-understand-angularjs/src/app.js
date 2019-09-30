1bbc8a2d04985b37cb9fdff4b503504c605644b9
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-30 15:55:32 -0300

    ADD email field validation

--- learn-understand-angularjs/src/app.js
+++ learn-understand-angularjs/src/app.js
@@ -1,7 +1,11 @@
-let app = angular.module('myApp', []);
+let app = angular.module('myApp', ['ngMessages']);
 
-app.controller('mainController', function($scope, $log) {
+app.controller('mainController', function($scope, $log, $filter) {
     
-$log.log("Hello.");
+$scope.name = 'Leonardo';
+$scope.formattedname = $filter('uppercase')($scope.name);
+
+$log.info($scope.name);
+$log.info($scope.formattedname);
 
 });
