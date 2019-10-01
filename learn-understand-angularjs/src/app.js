f2ad27da64c10cf1fedd15f6d825d97af233e294
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-01 10:39:50 -0300

    ADD setTimeOut with name example

--- learn-understand-angularjs/src/app.js
+++ learn-understand-angularjs/src/app.js
@@ -1,7 +1,10 @@
 let app = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-app.controller('mainController', function($scope, $log, $filter, $resource) {
-    
-console.log($resource)
+app.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
 
-});
+   $scope.name= 'Leo';
+
+   $timeout(function() {
+    $scope.name = 'Leonardo';
+   },3000);
+}]);
