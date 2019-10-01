8594ed0adc35b633fa539136ad51aeecd6aa29f1
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-01 15:21:32 -0300

    ADD new array object of rules

--- learn-understand-angularjs/src/app.js
+++ learn-understand-angularjs/src/app.js
@@ -8,4 +8,12 @@ app.controller('mainController', ['$scope', '$filter', function($scope, $filter)
     } 
 
     $scope.characters = 5;
+
+    $scope.rules = [
+        { rulename: "Must be 5 characters"},
+        { rulename: "Must not be used elsewhere"},
+        { rulename: "Must be cool"},
+    ];
+
+    console.log($scope.rules);
 }]);
