7b00fc75b91169e5401a32eef4d212b3aa9d87ba
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-01 14:51:23 -0300

    ADD ng-class and ng-show directives

--- learn-understand-angularjs/src/app.js
+++ learn-understand-angularjs/src/app.js
@@ -1,20 +1,11 @@
 let app = angular.module('myApp', []);
 
-app.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
+app.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
     $scope.handle = '';
 
     $scope.lowercaseLetter = function() {
         return $filter('lowercase')($scope.handle);
     } 
 
-    $scope.$watch('handle', function(newValue, oldValue) {
-        console.info('Changed!');
-        console.log('Old: ' +oldValue);
-        console.log('New: ' +newValue);
-    })
-
-    $timeout(function() {
-        $scope.handle = 'new';
-        console.log('Scope changed')
-    }, 3000)
+    $scope.characters = 5;
 }]);
