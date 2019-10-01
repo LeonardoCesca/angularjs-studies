afd76972b4c2dc44433ea24a93a9f94ab2105922
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-01 13:58:40 -0300

    ADD setTimeout es6

--- learn-understand-angularjs/src/app.js
+++ learn-understand-angularjs/src/app.js
@@ -6,4 +6,17 @@ app.controller('mainController', ['$scope', '$filter', function($scope, $filter)
     $scope.lowercaseLetter = function() {
         return $filter('lowercase')($scope.handle);
     } 
+
+    $scope.$watch('handle', function(newValue, oldValue) {
+        console.info('Changed!');
+        console.log('Old: ' +oldValue);
+        console.log('New: ' +newValue);
+    })
+
+    setTimeout(function() {
+        $scope.$apply(function() {
+            $scope.handle = 'new';
+            console.log('Scope changed')
+        })
+    }, 3000)
 }]);
