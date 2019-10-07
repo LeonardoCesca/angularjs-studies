e4cbd529d22820d48684c9af4daaae7d5b54622d
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-07 14:10:38 -0300

    ADD service to capture input

--- weather-forecast-spa/src/app.js
+++ weather-forecast-spa/src/app.js
@@ -16,10 +16,22 @@ weatherApp.config(function($routeProvider, $locationProvider) {
         })
 });
 
-weatherApp.controller('homeController', ['$scope', function() {
+weatherApp.service('cityService', function() {
+    this.city = 'New York, NY'
+});
+
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    })
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function() {
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
     
 }]);
