28ffa96bb782ba7deaf4403bd38491fabb9433ff
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-07 11:45:29 -0300

    ADD routes to navigate

--- weather-forecast-spa/src/app.js
+++ weather-forecast-spa/src/app.js
@@ -1,5 +1,19 @@
 let weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
+weatherApp.config(function($routeProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'src/pages/home.html',
+            controller: 'homeController'
+        })
+
+        .when('/forecast', {
+            templateUrl: 'src/pages/forecast.html',
+            controller: 'forecastController'
+        })
+});
+
 weatherApp.controller('homeController', ['$scope', function() {
 
 }]);
