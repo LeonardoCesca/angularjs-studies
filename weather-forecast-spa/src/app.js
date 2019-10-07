0c2cbf1ddffb42b3e6095eae22f78ce58c11acf2
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-07 11:16:54 -0300

    ADD controllers

--- weather-forecast-spa/src/app.js
+++ weather-forecast-spa/src/app.js
@@ -1,1 +1,9 @@
-let weatherApp = angular.module('weatherApp', []);  
+let weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.controller('homeController', ['$scope', function() {
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function() {
+    
+}]);
